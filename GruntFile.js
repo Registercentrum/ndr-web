5390ded0b33979a5cd3dcc5ea2cb6a69257c9191
Moa Nandorf <moa.nandorf@gmail.com>
2014-11-27 14:24:26 +0100

    Style updates

--- GruntFile.js
+++ GruntFile.js
@@ -11,19 +11,14 @@ module.exports = function(grunt) {
         },
 
         less: {
-            //development: {
-            //    options: {
-            //        paths: ["assets/css"]
-            //    },
-            //    files: {"css/ndr.css": "_concatenated-styles.less"}
-            //},
+
             production: {
                 options: {
-                    cleancss: true,
+                    cleancss: false,
                     ieCompat: true,
                     sourceMap: true,
-                    sourceMapFilename : "css/ndr.css.map",
-                    sourceMapURL: '/css/ndr.css.map' // the complete url and filename put in the compiled css file
+                    // sourceMapFilename : "css/ndr.css.map",
+                    // sourceMapURL: '/css/ndr.css.map' // the complete url and filename put in the compiled css file
                     // sourceMapBasepath: 'css', // Sets sourcemap base path, defaults to current working directory.
                     // sourceMapRootpath: '/' // adds this path onto the sourcemap filename and less file paths
                 },
@@ -38,7 +33,13 @@ module.exports = function(grunt) {
                 livereload: true
             },
 
-            files: ["src/components/*/*.less", "./less/*.less", "bower_components/bootstrap/less/*.less"],
+            files: [
+                "src/components/*/*.less",
+                "src/components/*/*.html",
+                "src/pages/*/*.html",
+                "./less/*.less",
+                "bower_components/bootstrap/less/*.less"
+            ],
             tasks: ["concat", "less"]
         }
 
