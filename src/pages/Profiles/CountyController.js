37c5432fdf096f8bd7a24434b8069051aabda3b3
lapidus <daniel@lapidus.se>
2014-11-28 16:43:07 +0100

    Additions to County profile

--- src/pages/Profiles/CountyController.js
+++ src/pages/Profiles/CountyController.js
@@ -1,19 +1,65 @@
 angular.module("ndrApp")
     .controller('CountyController',['$scope', '$stateParams', 'dataService', function($scope, $stateParams, dataService) {
 
-        console.log("CountyController Init:", $scope.data);
-        
+        console.log("CountyController Init ID:", $stateParams.id);
+
         var id = $stateParams.id;
 
         $scope.model = {
+            county : _.findWhere(dataService.data.counties,  {code : id}),
             id : id,
             data : {}
         }
 
-
         dataService.getOne("county", id).then(function (data){
             $scope.model.county = data;
         })
+
+
+        // GET DATA FOR BAR CHART
+        var query = queryFactory({});
+        dataService.getStats(query).then(function (data){
+
+            var series = [];
+            
+            _.each(data.statSet, function(obj, key){
+
+                var o = {
+                    name : obj.unit.name,
+                    color : obj.unit.levelID != id ? "#D4D4D4" : "#F1AD0F",
+                    y : obj.stat.r
+                }
+
+                series.push(o)
+            })
+            
+            $scope.model.data.hba1c = series;
+        })
+
+        // GET DATA FOR TREND CHART
+        var query = queryFactory({countyCode : id, interval : "m"});
+        dataService.getStats(query).then(function (data){
+
+            console.log("dd", data);
+            
+            var series = [];
+
+            _.each(data.statSet, function(obj, key){
+
+                var o = {
+                    name : obj.unit.name,
+                    color : obj.unit.levelID != id ? "#D4D4D4" : "#F1AD0F",
+                    y : obj.stat.r
+                }
+
+                series.push(o)
+            })
+
+            $scope.model.data.hba1c = series;
+
+        })
+
+
 
 /*
 
@@ -31,25 +77,7 @@ angular.module("ndrApp")
             });
 
 
-            var query = queryFactory();
-            dataService.getStats(query).then(function (data){
 
-                var series = [];
-
-                _.each(data.statSet, function(obj, key){
-
-                    var o = {
-                        name : obj.unit.name,
-                        //color : "#B4BCBE",
-                        y : obj.stat.r
-                    }
-
-                    series.push(o)
-                })
-
-                console.log("s", series);
-                $scope.model.data.hba1c = series;
-            })
 
         }
 
@@ -59,52 +87,36 @@ angular.module("ndrApp")
 */
 
 
-        var autocompleteList = [
-            {id: 1, title: 'Spectrometer'},
-            {id: 2, title: 'Star Chart'},
-            {id: 3, title: 'Laser Pointer'}
-        ];
 
 
 
-        $scope.config = {
-            valueField: 'id',
-            labelField: 'title',
-            delimiter: '|',
-           // maxItems: 1,
-            placeholder: 'Välj'
-            // maxItems: 1
+
+        function queryFactory(params){
+
+            var defaults = {
+                level: 1,
+                countyCode: 0,
+                unitID: 0,
+                indicatorID: 101,
+                fromYear: 2013,
+                fromQuartal: 0,
+                fromMonth: 0,
+                toYear: 2014,
+                toQuartal: 0,
+                toMonth: 0,
+                diabetesTypeCode: 1,
+                sex: 0,
+                unitTypeID: 0,
+                fromAge: 0,
+                toAge: 0,
+                interval: null,
+                recalculate: false,
+                outdatedDays: 14
+            }
+
+            return angular.extend(defaults, params)
+
         }
-
-
-        function queryFactory(){
-
-
-            return {
-
-                    level: 1,
-                    countyCode: 0,
-                    unitID: 0,
-                    indicatorID: 101,
-                    fromYear: 2013,
-                    fromQuartal: 0,
-                    fromMonth: 0,
-                    toYear: 2013,
-                    toQuartal: 0,
-                    toMonth: 0,
-                    diabetesTypeCode: 1,
-                    sex: 0,
-                    unitTypeID: 0,
-                    fromAge: 0,
-                    toAge: 0,
-                    interval: null,
-                    recalculate: false,
-                    outdatedDays: 14
-            };
-        }
-
-
-
 
 
 
