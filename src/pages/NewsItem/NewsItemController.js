0bd420752cf91157c7605a01d55c087be858b491
Moa Nandorf <moa.nandorf@gmail.com>
2014-12-10 11:46:31 +0100

    basic news styling

--- src/pages/NewsItem/NewsItemController.js
+++ src/pages/NewsItem/NewsItemController.js
@@ -2,7 +2,11 @@ angular.module("ndrApp")
     .controller('NewsItemController',['$scope', '$stateParams', 'dataService', function($scope, $stateParams, dataService) {
 
         dataService.getOne("news", $stateParams.id).then(function (data){
-            $scope.newsItem = data;
+            $scope.article = data;
+        })
+
+        dataService.getList("news").then(function (data){
+            $scope.news = data;
         })
 
     }]);
