a09ef7d848aab2858c12107a791903f292d4719b
Moa Nandorf <moa.nandorf@gmail.com>
2014-12-10 14:06:34 +0100

    Sidebar news on single news item

--- src/pages/NewsItem/NewsItemController.js
+++ src/pages/NewsItem/NewsItemController.js
@@ -1,12 +1,22 @@
 angular.module("ndrApp")
     .controller('NewsItemController',['$scope', '$stateParams', 'dataService', function($scope, $stateParams, dataService) {
 
+        $scope.model = {};
+
         dataService.getOne("news", $stateParams.id).then(function (data){
             $scope.article = data;
         })
 
         dataService.getList("news").then(function (data){
-            $scope.news = data;
+            data = data.splice(0,3);
+
+            angular.forEach(data, function(item) {
+                item.link = '/#/nyheter/' + item.newsID;
+            });
+            
+            $scope.model.listModelNews = {
+                data : data
+            }
         })
 
     }]);
