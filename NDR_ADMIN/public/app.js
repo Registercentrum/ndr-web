5b736d1eb9cac893ce374439ec0139eb748fbb1d
HenrikMilefors <henrik.milefors@registercentrum.se>
2015-03-17 09:43:12 +0100

    Update NG Admin 0.6 and more

--- NDR_ADMIN/public/app.js
+++ NDR_ADMIN/public/app.js
@@ -3,7 +3,8 @@
     "use strict";
 
     var app = angular.module('myApp', ['ng-admin']);
-
+	
+	
     app.directive('customPostLink', ['$location', function ($location) {
         return {
             restrict: 'E',
@@ -12,7 +13,7 @@
                 $scope.displayPost = function (entry) {
                     var postId = entry.values.newsID;
 
-                    console.log(entry);
+                    //console.log(entry);
 
                     $location.path('/edit/posts/' + postId);
                 };
@@ -20,9 +21,10 @@
         };
     }]);
 	
-    app.config(function (NgAdminConfigurationProvider, Application, Entity, Field, Reference, ReferencedList, ReferenceMany, RestangularProvider, $stateProvider) {
+    app.config(function (NgAdminConfigurationProvider, RestangularProvider, $stateProvider) {
         var nga = NgAdminConfigurationProvider;
 
+		
         function truncate(value) {
             
 			if(value === '')
@@ -36,7 +38,14 @@
         }
 		
         RestangularProvider.setDefaultRequestParams({APIKey: 'jEGPvHoP7G4eMkjLQwE5'});
+				
+        var app = nga.application('NDR Admin') // application main title
+			//.baseApiUrl('https://w8-038.rcvg.local/api/'); // main API endpoint, Henrik utveckling
+            .baseApiUrl('https://ndr.registercentrum.se/api/'); // main API endpoint
 		
+<<<<<<< HEAD
+        var news = nga.entity('News')
+=======
 		// RestangularProvider.addElementTransformer('news', function(element) {
 			// console.log('test');
 			
@@ -61,71 +70,108 @@
 		console.log(app);
 		
         var news = new Entity('News')
+>>>>>>> origin/dev
             .identifier(nga.field('newsID'))
 			.label('Nyheter');
 		
-        var publications = new Entity('ResearchProject')
+        var publications = nga.entity('ResearchProject')
              .identifier(nga.field('id'))
 			 .label('Forskningsprojekt');
 			 
-        var units = new Entity('Unit')
+        var units = nga.entity('Unit')
              .identifier(nga.field('unitID'))
 			 .label('Enheter');
 			 
-        var users = new Entity('User')
+        var users = nga.entity('User')
              .identifier(nga.field('userID'))
 			 .label('Användare');
 		
-		var newsCategories = new Entity('NewsCategory')
+		var accounts = nga.entity('Account')
+			.identifier(nga.field('accountID'))
+			.label('Användarkonton');
+		
+		var newsCategories = nga.entity('NewsCategory')
             .readOnly(); // a readOnly entity has disabled creation, edition, and deletion views
 			
-		var unitTypes = new Entity('UnitType')
+		var unitTypes = nga.entity('UnitType')
             .readOnly(); // a readOnly entity has disabled creation, edition, and deletion views
 			
-		var counties = new Entity('County')
+		var counties = nga.entity('County')
             .readOnly() // a readOnly entity has disabled creation, edition, and deletion views
 			.identifier(nga.field('code'));
 		
         // set the application entities
-        app.addEntity(news);
-		app.addEntity(units);
-		app.addEntity(users);
-		app.addEntity(publications);
-
-        users.dashboardView()
-            .title('Användare')
-			//.limit(20)
-            // .order(1) // display the comment panel second in the dashboard
-            .fields([
-                nga.field('userID').label('ID').order(0),
-				nga.field('hsaid').label('HSAID').map(truncate).order(1),
-                nga.field('firstName').label('Förnamn').map(truncate).order(1),
-				nga.field('lastName').label('Efternamn').map(truncate).order(1)
-                // new Field() // template fields don't need a name in dashboard view
-                //     .type('template') // a field which uses a custom template
-                //     .label('Actions')
-                //     .template('<custom-post-link></custom-post-link>') // you can use custom directives, too
-                //     .order(2)
-            ]);		
+        app.addEntity(news)
+			.addEntity(units)
+			.addEntity(users)
+			.addEntity(accounts)
+			.addEntity(publications);
 		
-       users.listView()
-            .title('Användare')
-			//.limit(30)
-			.sortField('hsaid')
-			//.infinitePagination(false)
-			.perPage(50)
-            // .order(1) // display the comment panel second in the dashboard
+		// ****** ACCOUNTS ******
+        accounts.dashboardView()
+             .title('Användarkonton')
+             .fields([
+                 nga.field('unitName').label('Enhetsnamn').map(truncate),
+				 nga.field('hsaid').label('HSAID').map(truncate)
+             ]);
+
+        accounts.listView()
+             .title('Användarkonton')
+			 .perPage(50)
+             .fields([
+				nga.field('unitID').label('Enhets-ID').map(truncate),
+                 nga.field('unitName').label('Enhetsnamn').map(truncate),
+				 nga.field('hsaid').label('HSAID').map(truncate)
+             ])
+			.filters([
+				nga.field('q').label('Sök')
+			])
+			.listActions(['edit']);
+		
+        accounts.creationView()
+			.title('Nytt användarkonto')
             .fields([
-                nga.field('userID').label('ID').order(0),
-				//nga.field('hsaid').label('HSAID').map(truncate),
-                nga.field('firstName').label('Förnamn'),
+				nga.field('unitID').label('Enhets-ID').editable(false),
+				nga.field('userID').label('Användar-ID'),
+				nga.field('statusID', 'choice').label('Status')
+					.choices(
+						[{value: '1', label: 'Aktiv'},
+						{value: '2', label: 'Second'}])
+            ]);
+		
+        accounts.editionView()
+			.title('Uppdatera användarkonto')
+            .fields([
+				nga.field('statusID', 'choice').label('Status')
+					.choices(
+						[{value: '1', label: 'First'},
+						{value: '2', label: 'Second'}])
+            ]);	
+		
+		
+		// ****** USER ******
+		users.dashboardView()
+			.title('Användare')
+			.fields([
+				nga.field('userID').label('ID').order(0),
+				nga.field('hsaid').label('HSAID').map(truncate).order(1),
+				nga.field('firstName').label('Förnamn').map(truncate).order(1),
+				nga.field('lastName').label('Efternamn').map(truncate).order(1)
+			]);		
+		
+		users.listView()
+			.title('Användare')
+			.sortField('hsaid')
+			.perPage(50)
+			.fields([
+				nga.field('firstName').label('Förnamn'),
 				nga.field('lastName').label('Efternamn'),
 				nga.field('email','email').label('E-post'),
-				nga.field('isKas','boolean').label('KAS'),
+				nga.field('isKAS','boolean').label('KAS'),
 				nga.field('isCoordinator','boolean').label('Koordinator'),
 				nga.field('isAdministrator','boolean').label('Administratör')
-            ])
-            .listActions(['edit'])
+			])
+			.listActions(['edit'])
 			.filters([
 				nga.field('q').label('Sök'),
 			]);
@@ -133,13 +179,13 @@
         users.creationView()
 			.title('Ny användare')
             .fields([
-				nga.field('hsaid').label('HSAID').map(truncate),
+				nga.field('hsaid').label('HSAID').editable(false).map(truncate),
                 nga.field('firstName').label('Förnamn').map(truncate),
 				nga.field('lastName').label('Efternamn').map(truncate),
 				nga.field('workTitle').label('Titel').map(truncate),
 				nga.field('email','email').label('E-post').map(truncate),
 				nga.field('organization').label('Organisation').map(truncate),
-				nga.field('isKas','boolean').label('KAS').map(truncate),
+				nga.field('isKAS','boolean').label('KAS').map(truncate),
 				nga.field('isCoordinator','boolean').label('Koordinator').map(truncate),
 				nga.field('isAdministrator','boolean').label('Administratör').map(truncate)
             ]);
@@ -148,33 +194,23 @@
 			.title('Uppdatera användare')
 			.description('"{{ entry.values.hsaid }}"')
             .fields([
-                //new Field('newsID'),
                 users.creationView().fields()
             ]);	
 		
+		// ****** UNIT ******
         units.dashboardView()
             .title('Enheter')
-			//.limit(20)
-            // .order(1) // display the comment panel second in the dashboard
             .fields([
-                nga.field('unitID').label('ID').order(0),
+                nga.field('unitID').label('NDR-ID').order(0),
                 nga.field('name').label('Enhetsnamn').map(truncate).order(1)
-                // new Field() // template fields don't need a name in dashboard view
-                //     .type('template') // a field which uses a custom template
-                //     .label('Actions')
-                //     .template('<custom-post-link></custom-post-link>') // you can use custom directives, too
-                //     .order(2)
             ]);
 			
         units.listView()
             .title('Enheter')
-			//.limit(30)
 			.sortField('name')
-			//.infinitePagination(true)
 			.perPage(50)
-            // .order(1) // display the comment panel second in the dashboard
             .fields([
-                nga.field('unitID').label('ID').order(0),
+                nga.field('unitID').label('NDR-ID').order(0),
                 nga.field('name').label('Enhetsnamn').map(truncate).order(1),
 				nga.field('hsaid').label('HSAID').map(truncate).order(2),
 				nga.field('manager').label('Verksamhetschef').map(truncate).order(2),
@@ -185,32 +221,14 @@
 				nga.field('q').label('Sök'),
 				nga.field('countyCode', 'reference')
 					.label('Landsting')
-					//.cssClasses('medium')
-					//.map(truncate) // Allows to truncate values in the select
 					.targetEntity(counties) // Select a target Entity
 					.targetField(nga.field('name')), // Select a label Field
 				nga.field('typeID', 'reference')
 					.label('Typ')
-					//.choices([{value: 1, label: 'Primärvårdsenhet'},{value: 2, label: 'Medicinklinik'}])
-					//.map(truncate) // Allows to truncate values in the select
 					.targetEntity(unitTypes) // Select a target Entity
 					.targetField(nga.field('name')), // Select a label Field
 				nga.field('notActive', 'boolean').label('Visa inaktiva')
 			]);
-		
-        news.dashboardView()
-            .title('Nyheter')
-            // .order(1) // display the comment panel second in the dashboard
-            // .limit(5)
-            .fields([
-                nga.field('newsID').label('ID').order(0),
-                nga.field('title').label('Rubrik').map(truncate).order(1)
-                // new Field() // template fields don't need a name in dashboard view
-                //     .type('template') // a field which uses a custom template
-                //     .label('Actions')
-                //     .template('<custom-post-link></custom-post-link>') // you can use custom directives, too
-                //     .order(2)
-            ]);
 
         units.creationView()
 			.title('Ny')
@@ -246,6 +264,14 @@
                 units.creationView().fields()
             ]);	
 			
+		// ****** NEWS ******
+        news.dashboardView()
+            .title('Nyheter')
+            .fields([
+                nga.field('newsID').label('ID').order(0),
+                nga.field('title').label('Rubrik').map(truncate).order(1)
+            ]);
+
         news.listView()
             .title('Nyheter')
 			.sortField('priority')
@@ -269,7 +295,7 @@
 				nga.field('includeOutsidePeriod', 'boolean').label('Inkludera opublicerat'),
 				nga.field('isInternal', 'boolean').label('Visa endast interna nyheter')
 			]);	
-			
+		
         news.creationView()
 			.title('Ny')
             .fields([
@@ -287,9 +313,6 @@
 					.label('Kategorier')
 					.targetEntity(newsCategories) // the tag entity is defined later in this file
 					.targetField(nga.field('name')), // the field to be displayed in this list
-				//new ReferenceMany('categories') // a Reference is a particular type of field that references another entity
-				//	.targetEntity(newsCategories) // the tag entity is defined later in this file
-				//	.targetField(nga.field('name')), // the field to be displayed in this list
                 nga.field('author').label('Av'),
                 nga.field('title').label('Titel'),
                 nga.field('image').label('Bild'),
@@ -303,22 +326,15 @@
 			.title('Uppdatera')
 			.description('"{{ entry.values.title }}"')
             .fields([
-                //new Field('newsID'),
                 news.creationView().fields()
             ]);
-			
+		
+		// ****** PUBLICATIONS ******
         publications.dashboardView()
              .title('Forskningsprojekt')
-             // .order(1) // display the comment panel second in the dashboard
-             // .limit(5)
              .fields([
                  nga.field('id').label('ID').order(0),
                  nga.field('name').map(truncate).order(1)
-                 // new Field() // template fields don't need a name in dashboard view
-                 //     .type('template') // a field which uses a custom template
-                 //     .label('Actions')
-                 //     .template('<custom-post-link></custom-post-link>') // you can use custom directives, too
-                 //     .order(2)
              ]);
 
         publications.listView()
@@ -329,33 +345,75 @@
              ])
              .listActions(['edit', 'delete']);
 
-		
         publications.creationView()
-		.title('Ny')
-		.fields([
-			nga.field('name').label('Titel'),
-			nga.field('laymansDescription').type('text').label('Beskrivning'),
-			nga.field('firstAuthor').label('Första författare'),
-			nga.field('lastAuthor').label('Sista Författare'),
-			nga.field('otherAuthor').label('Andra Författare'),
-			nga.field('dateOfSubmission','date').label('Submitteringssdatum'),
-			nga.field('dateOfPublication','date').label('Publiceringsdatum'),
-			nga.field('magazine').label('Tidning'),
-			nga.field('issue').label('Upplaga'),
-			nga.field('pages').label('Sidor'),
-			nga.field('image').label('Bild'),
-			nga.field('pubMedURL').label('Länk till pubmed')
-		]);
-
+			.title('Ny')
+			.fields([
+				nga.field('name').label('Titel'),
+				nga.field('laymansDescription','text').label('Beskrivning'),
+				nga.field('firstAuthor').label('Första författare'),
+				nga.field('lastAuthor').label('Sista Författare'),
+				nga.field('otherAuthor').label('Andra Författare'),
+				nga.field('dateOfSubmission','date').label('Submitteringssdatum'),
+				nga.field('dateOfPublication','date').label('Publiceringsdatum'),
+				nga.field('magazine').label('Tidning'),
+				nga.field('issue').label('Upplaga'),
+				nga.field('pages').label('Sidor'),
+				nga.field('image').label('Bild'),
+				nga.field('pubMedURL').label('Länk till pubmed')
+			]);
+		
         publications.editionView()
 			.title('Uppdatera')
 			.description('"{{ entry.values.name }}"')
             .fields([
-                //new Field('id'),
                 publications.creationView().fields()
             ]);
+		
 
-
-        nga.configure(app);
+		// RestangularProvider.addElementTransformer('Account', true, function(elements) {
+			
+			// //do my stuff here
+			// for (var i = 0, len = elements.length; i < len; i++) {
+			  // elements[i].userID=elements[i].user.userID;
+			  // elements[i].hsaid=elements[i].user.hsaid;
+			  // elements[i].unitID=elements[i].unit.unitID;
+			  // elements[i].unitName=elements[i].unit.name;
+			// }
+			// //console.log(elements);
+			
+			// return elements;
+		// });
+		RestangularProvider.addElementTransformer('Account', function(elements) {
+			
+			//do my stuff here
+			if (elements.length>0) {
+				for (var i = 0, len = elements.length; i < len; i++) {
+				  elements[i].userID=elements[i].user.userID;
+				  elements[i].hsaid=elements[i].user.hsaid;
+				  elements[i].unitID=elements[i].unit.unitID;
+				  elements[i].unitName=elements[i].unit.name;
+				}
+			} else {
+				//elements.userID=elements.user.userID;
+				//elements.hsaid=elements.user.hsaid;
+				//elements.unitID=elements.unit.unitID;
+				//elements.unitName=elements.unit.name;
+			}
+			
+			return elements;
+		});
+		
+		
+		//todo transformera nyheter för att skapa array av kategori-idn .categories  ex. = [1,2]
+		RestangularProvider.addElementTransformer('News', true, function(elements) {
+			
+			for (var i = 0, len = elements.length; i < len; i++) {
+			  elements[i].categories=[1,2];
+			}
+			
+			return elements;
+		});
+		
+        NgAdminConfigurationProvider.configure(app);
     });
 }());
