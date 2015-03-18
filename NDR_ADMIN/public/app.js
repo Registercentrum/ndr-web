232fb42b914ff986ba3ca8aa5414d27f10d2d44b
HenrikMilefors <henrik.milefors@registercentrum.se>
2015-03-18 15:15:44 +0100

    API adress corrected

--- NDR_ADMIN/public/app.js
+++ NDR_ADMIN/public/app.js
@@ -45,8 +45,8 @@
         var news = nga.entity('News')
 		
         var app = nga.application('NDR Admin') // application main title
-			.baseApiUrl('https://w8-038.rcvg.local/api/'); // main API endpoint
-            //.baseApiUrl('https://ndr.registercentrum.se/api/'); // main API endpoint
+			//.baseApiUrl('https://w8-038.rcvg.local/api/'); // main API endpoint
+            .baseApiUrl('https://ndr.registercentrum.se/api/'); // main API endpoint
 			
         var news = nga.entity('News')
             .identifier(nga.field('newsID'))
