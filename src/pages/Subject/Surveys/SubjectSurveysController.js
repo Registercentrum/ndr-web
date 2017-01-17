aa572c686e43cc7660534c80a333a82911f3bb87
Arek Mytych <amytych@gmail.com>
2017-01-17 17:11:25 +0100

    Transitions and the preamble for survey form
    + other small fixes

--- src/pages/Subject/Surveys/SubjectSurveysController.js
+++ src/pages/Subject/Surveys/SubjectSurveysController.js
@@ -1,7 +1,6 @@
 angular.module("ndrApp")
   .controller('SubjectSurveysController',
-    ['$scope', '$q', '$stateParams', '$state', '$log', '$filter', 'dataService', '$timeout', '$http', 'APIconfigService',
-    function ($scope,   $q,   $stateParams,   $state,   $log,   $filter,   dataService, $timeout, $http, APIconfigService) {
+    ['$scope', 'dataService', function ($scope, dataService) {
 
     var survey = $scope.accountModel.subject.invites[0];
 
@@ -17,32 +16,30 @@ angular.module("ndrApp")
     dataService.getPROMFormMeta()
       .then(function (response) {
         $scope.model.PROMFormMeta = response.data;
-        $scope.model.activeQuestion =
-          response.data[0].questiongroups[0].questions[0].questionID
         $scope.model.questionsCount = getQuestionsCount(response.data);
-        console.log("cccc", $scope.model.questionsCount)
-        console.log("mmmm", response.data);
       });
 
-    $scope.handleAnswerChange = function (name, value) {
-      // console.log($scope.model.answers[name], value)
-      // if (+$scope.model.answers[name] === +value) {
-      //   console.log(name, value)
-      //   $scope.model.answers[name] = null;
-      // }
-      // console.log($scope.model.answers);
+    $scope.handleAnswerChange = function ($event) {
+      var name = $event.target.name;
+      var value = $event.target.value;
+      $scope.model.answers[name] = +$scope.model.answers[name] === +value ? null : value;
       $scope.model.answersCount = getAnswersCount()
-      $scope.setActiveQuestion(
-        $scope.model.activeQuestion + 1
-      )
+      $scope.setActiveQuestion($scope.model.activeQuestion + 1)
       dataService.savePROMForm(survey.inviteID, survey.key, $scope.model.answers);
     }
 
     $scope.setActiveQuestion = function (id) {
-      $scope.model.activeQuestion = id;
+
+      var $el = $("#question-" + id);
       $("html, body").animate({
-        scrollTop: $("#question-" + id).offset().top - 20
-      }, 500);
+          scrollTop: $el.offset().top + ($el.outerHeight() / 2) - ($(window).height() / 2)
+        },
+        500,
+        function () {
+          $scope.$apply(function () {
+            $scope.model.activeQuestion = id;
+          });
+        });
     }
 
     function getQuestionsCount (meta) {
@@ -63,5 +60,29 @@ angular.module("ndrApp")
       }
       return count;
     }
-}])
+
+    $(window).scroll(function () {
+      var scrollTop = $(document).scrollTop() + ($(window).height() / 2);
+      var positions = [];
+      var index = 0;
+
+      $('.question-holder').each(function(){
+        var $this = $(this);
+        positions.push({
+          position: $this.offset().top + $this.outerHeight() / 2,
+          id: +this.id.split("-")[1]
+        });
+      });
+
+      for (var i = positions.length - 1; i >= 0; i--) {
+        if (Math.abs(scrollTop - positions[i].position) < Math.abs(scrollTop - positions[index].position)) {
+          index = i;
+        }
+      }
+
+      $scope.$apply(function () {
+        $scope.model.activeQuestion = +positions[index].id;
+      })
+    });
+  }])
 
