4cb1f4240e0a1dfcea70ca589be51896658640d6
Arek Mytych <amytych@gmail.com>
2017-01-23 14:31:34 +0100

    Patient profile

--- src/components/interactive/highcharts/lineChart.js
+++ src/components/interactive/highcharts/lineChart.js
@@ -1,138 +1,117 @@
 angular.module('ndrApp')
-    .directive('lineChart', [function() {
+.directive('lineChart', [function() {
+  return {
+    restrict: 'A',
+    template : "<div class='chart-container'></div>",
+    scope: {
+      model: "=",
+      yMin: "@",
+      yMax: "@",
+      dateFormat: "@",
+      height: "@",
+      color: "@"
+    },
+    link: function(scope, element, attrs) {
+      var dateFormat = _.isString(scope.dateFormat) ? scope.dateFormat : '%Y';
+      var color = scope.color || "#E14274";
 
-        return {
-            restrict: 'A',
-            template : "<div class='chart-container'></div>",
-
-            scope: {
-                model: "="
+      var chart = jQuery(".chart-container", element).highcharts({
+          chart: {
+            type: 'area',
+            height : !_.isUndefined(scope.height) && _.isNumber(+scope.height) ? +scope.height : 140,
+            marginTop : 5,
+            marginBottom : 30,
+            marginLeft : 35,
+            marginRight: 5,
+            spacingLeft : 30
+          },
+          title: {
+            text: ''
+          },
+          xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
+              year: '%Y',
+              day: dateFormat
             },
+            lineWidth: 0,
+            gridLineWidth: 0,
+            minorGridLineWidth: 0,
+            lineColor: '#eee',
+            labels: {
 
-            // set up the isolate scope so that we don't clobber parent scope
-            link: function(scope, element, attrs) {
-
-
-                var chart = jQuery(".chart-container", element).highcharts({
-                        chart: {
-                            type: 'area',
-                            height : 140,
-                            marginTop : 5,
-                            marginBottom : 30,
-                            marginLeft : 35,
-                            marginRight: 5,
-                            spacingLeft : 30
-                        },
-                        title: {
-                            text: ''
-                        },
-
-
-                        xAxis: {
-
-
-                            type: 'datetime',
-                           /* dateTimeLabelFormats: { // don't display the dummy year
-                                //month: '%e. %b',
-                                year: '%b'
-                            },*/
-
-                            dateTimeLabelFormats: {
-                                year: '%Y'
-                            },
-
-                            lineWidth: 0,
-                            gridLineWidth: 0,
-                            minorGridLineWidth: 0,
-                            lineColor: '#eee',
-                            labels: {
-                                //enabled: false
-                            },
-                            minorTickLength: 0,
-                            tickLength: 0
-
-                        },
-
-                        yAxis: {
-                           gridLineWidth: 1,
-                           gridLineColor : "#eee",
-                           lineWidth: 0,
-
-                            minorGridLineWidth: 0,
-                            min : 40,
-                            max : 70,
-
-                            title: {
-                                text: '',
-                                align: 'high'
-                            },
-                            labels: {
-                                align : "right",
-                                //x : 20,
-                                //y : -8,
-
-                            }
-
-
-                        },
-                         tooltip: {
-                            formatter: function() {
-                                var time = Highcharts.dateFormat('%Y', new Date(this.x))
-                                return time + ': <b>' +  this.y + '</b>';
-                            }
-                        },
-                        plotOptions: {
-                            bar: {
-                                dataLabels: {
-                                    enabled: true
-                                }
-                            },
-                            area : {
-
-                                fillColor : {
-                                    linearGradient: {
-                                        x1: 0,
-                                        y1: 0,
-                                        x2: 0,
-                                        y2: 1
-                                    },
-                                    stops: [
-                                        [0, Highcharts.Color("#E14274").setOpacity(0.5).get('rgba')],
-                                        [1, Highcharts.Color("#E14274").setOpacity(0).get('rgba')]
-                                    ]
-                                }
-                            }
-                        },
-                        legend: {
-                            enabled: false,
-                            layout: 'vertical',
-                            align: 'right',
-                            verticalAlign: 'top',
-                            x: -40,
-                            y: 100,
-                            floating: true,
-                            borderWidth: 1,
-                            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor || '#FFFFFF'),
-                            shadow: true
-                        },
-                        credits: {
-                            enabled: false
-                        },
-                        series: [{
-                            dashStyle: "ShortDot",
-                            color : "#E14274",
-                            name: 'Värde',
-                            data : [{x: 1, y:10}, {x: 3, y:5}]
-
-                        }]
-                    });
-
-
-                scope.$watch('model', function(model) {
-
-                    chart.highcharts().series[0].setData(angular.copy(model))
-
-                }, true);
+            },
+            minorTickLength: 0,
+            tickLength: 0
+          },
+          yAxis: {
+            gridLineWidth: 1,
+            gridLineColor : "#eee",
+            lineWidth: 0,
+            minorGridLineWidth: 0,
+            min : !_.isUndefined(scope.yMin) && _.isNumber(+scope.yMin) ? +scope.yMin : 40,
+            max : !_.isUndefined(scope.yMax) && _.isNumber(+scope.yMax) ? +scope.yMax : 70,
+            title: {
+              text: '',
+              align: 'high'
+            },
+            labels: {
+              align : "right",
             }
-        };
-    }]);
+          },
+          tooltip: {
+            formatter: function () {
+              var time = Highcharts.dateFormat(dateFormat, new Date(this.x));
+              return time + ': <b>' +  this.y + '</b>';
+            }
+          },
+          plotOptions: {
+            bar: {
+              dataLabels: {
+                enabled: true
+              }
+            },
+            area : {
+              fillColor : {
+                linearGradient: {
+                  x1: 0,
+                  y1: 0,
+                  x2: 0,
+                  y2: 1
+                },
+                stops: [
+                  [0, Highcharts.Color(color).setOpacity(0.5).get('rgba')],
+                  [1, Highcharts.Color(color).setOpacity(0).get('rgba')]
+                ]
+              }
+            }
+          },
+          legend: {
+            enabled: false,
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -40,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor || '#FFFFFF'),
+            shadow: true
+          },
+          credits: {
+            enabled: false
+          },
+          series: [{
+            dashStyle: "ShortDot",
+            color : color,
+            name: 'Värde',
+            data : [{x: 1, y:10}, {x: 3, y:5}]
+          }]
+        });
+
+      scope.$watch('model', function (model) {
+        chart.highcharts().series[0].setData(angular.copy(model));
+      }, true);
+    }
+  };
+}]);
--- src/pages/Subject/Profile/SubjectProfileController.js
+++ src/pages/Subject/Profile/SubjectProfileController.js
@@ -3,9 +3,72 @@ angular.module("ndrApp")
         ['$scope', '$q', '$stateParams', '$state', '$log', '$filter', 'dataService', '$timeout', '$http', 'APIconfigService',
         function ($scope,   $q,   $stateParams,   $state,   $log,   $filter,   dataService, $timeout, $http, APIconfigService) {
 
-        var Account = $scope.accountModel;
+        $scope.subject = $scope.accountModel.subject;
 
-        console.log(Account);
+        // update subject's age
+        $scope.subject.age = moment().diff(moment($scope.subject.dateOfBirth), 'years');
+        // update subject's debut
+        $scope.subject.debut = moment().diff(moment([$scope.subject.yearOfOnset, 0, 1]), 'years');
 
-}])
+        // get submitted surveys
+        var submitted = _.filter($scope.subject.invites, function (i) { return !!i.submittedAt; });
+        if (submitted.length) submitted = _.sortBy(submitted, function (s) { return +(new Date(s.submittedAt)); })
+        // group by dates
+        $scope.subject.surveys = !submitted.length ?
+          [] :
+          _.map(submitted[0].outcomes, function (outcome) {
+            var datum = {
+              dimension: outcome.dimension
+            };
+            datum.series = _.map(submitted, function (s) {
+              return {
+                x: +(new Date(s.submittedAt)),
+                y: _.find(s.outcomes, function (o) {
+                  return o.dimension.id === outcome.dimension.id;
+                }).outcome
+              };
+            });
+            datum.latestOutcome = _.last(datum.series).y
+            datum.diffFromPrevious = datum.series.length > 1 && _.isNumber(datum.latestOutcome) && _.isNumber(datum.series[datum.series.length - 2].y) ?
+              datum.latestOutcome - datum.series[datum.series.length - 2].y :
+              null;
+            return datum;
+          });
+
+        // get series for the profile charts
+        var charts = [{
+            title: 'HbA1c',
+            series: getSeries('hba1c'),
+          }, {
+            title: 'BMI',
+            series: getSeries('bmi'),
+          }, {
+            title: 'LDL',
+            series: getSeries('ldl'),
+          }, {
+            title: 'Blodtryck',
+            series: getSeries('bpSystolic'),
+          }
+        ];
+
+        $scope.subject.charts = _.map(charts, function (chart) {
+          chart.latest = _.last(chart.series).y;
+          chart.yMax = _.max(chart.series, function (s) { return s.y; }).y;
+          return chart;
+        });
+
+        function getSeries (type) {
+          return _.map(
+            _.sortBy($scope.subject.contacts, function (c) { return +(new Date(c.contactDate)); }),
+            function (contact) {
+              return {
+                x: +(new Date(contact.contactDate)),
+                y: contact[type]
+              };
+            }
+          );
+        }
+
+        console.log($scope.subject);
+}]);
 
