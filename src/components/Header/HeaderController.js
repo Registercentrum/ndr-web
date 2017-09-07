1032a37e1cd3b75203f7b430bc929fc199fa084d
HenrikMilefors <henrik.milefors@registercentrum.se>
2017-09-07 15:48:39 +0200

    Header info personalized
    Depending on new user being recognized with SITHS-card the info path
    applying for account is shortened.

--- src/components/Header/HeaderController.js
+++ src/components/Header/HeaderController.js
@@ -1,9 +1,23 @@
 'use strict';
 
 angular.module('ndrApp')
-  .controller('HeaderController', [
-             '$scope', 'accountService',
-    function ($scope,   accountService) {
-      $scope.accountService = accountService;
-      $scope.accountModel = accountService.accountModel;
-    }]);
+.controller('HeaderController', [
+		 '$scope', 'accountService', 'cookieFactory',
+function ($scope,   accountService, cookieFactory) {
+	$scope.accountService = accountService;
+	$scope.accountModel = accountService.accountModel;
+	
+	var cookieKey = 'cookieConsent';
+  
+  	var readCookieConsent = function()  {
+		$scope.accountModel.cookieConsent = cookieFactory.read(cookieKey);
+	};
+  
+	$scope.setCookieConsent = function() {
+		var minutes = 60*24*365*20; //20 år
+		cookieFactory.create(cookieKey,1,minutes);
+		readCookieConsent();
+	};
+
+	readCookieConsent();
+}]);
