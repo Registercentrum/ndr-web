9f5b0abfdededa6cbb55c32791e25ca24c8fee59
HenrikMilefors <henrik.milefors@registercentrum.se>
2018-04-10 09:41:37 +0200

    Reset fetched metafields when activeaccount is changed

--- src/components/Header/HeaderController.js
+++ src/components/Header/HeaderController.js
@@ -2,26 +2,26 @@
 
 angular.module('ndrApp')
 .controller('HeaderController', [
-		 '$scope', 'accountService', 'cookieFactory', 'dataService',
-function ($scope,   accountService, cookieFactory, dataService) {
+		 '$scope', '$state', 'accountService', 'cookieFactory', 'dataService',
+function ($scope, $state, accountService, cookieFactory, dataService) {
 	$scope.accountService = accountService;
 	$scope.accountModel = accountService.accountModel;
-	
+
 	var cookieKey = 'cookieConsent';
-  
+
   	var readCookieConsent = function()  {
 		$scope.accountModel.cookieConsent = cookieFactory.read(cookieKey);
 	};
-  
+
 	$scope.setCookieConsent = function() {
 		var minutes = 60*24*365*20; //20 år
 		cookieFactory.create(cookieKey,1,minutes);
 		readCookieConsent();
 	};
-	
+
 	$scope.setAccount = function(accountID) {
 		accountService.updateAccount(accountID)
-		dataService.setValue('optionalQuestions', null);
+		dataService.setValue('metafields', null);
 	};
 
 	readCookieConsent();
