afb92c5b568f6214df20808eb553d11d6f541ccd
HenrikMilefors <henrik.milefors@registercentrum.se>
2018-04-10 09:50:04 +0200

    Changes due to report form being loaded from metadata

--- src/pages/account/Report/ReportController.js
+++ src/pages/account/Report/ReportController.js
@@ -1,225 +1,301 @@
 'use strict';
 
 angular.module('ndrApp')
-    .controller('ModalInstanceCtrl', ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+    .controller('ModalInstanceCtrl', ['$scope', '$modalInstance', '$state', function($scope, $modalInstance, $state) {
 
-        $scope.close = function () {
-            $modalInstance.dismiss('cancel');
+        $scope.reload = function() {
+          console.log('from close modal', $scope);
+          $scope.cancel();
+          $state.reload();
+          //location.reload();
+          //$modalInstance.dismiss('cancel');
+          //location.reload();
         };
 
-        $scope.clear = function () {
-          $('html, body').animate({scrollTop: 0}, 500, function () {
-			var socinput = document.getElementById('socialnumber-input');
-			if (socinput != null)
-				window.document.getElementById('socialnumber-input').focus();
-          });
-          $modalInstance.dismiss('cancel');
-		  
-		  $scope.__proto__.subject = null;
-		  $scope.__proto__.socialnumber = null;
+        $scope.cancel = function() {
+          $modalInstance.close();
+          //$modalInstance.dismiss('cancel');
+        };
+
+        $scope.clear = function() {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 500, function() {
+                var socinput = document.getElementById('socialnumber-input');
+                if (socinput != null)
+                    window.document.getElementById('socialnumber-input').focus();
+            });
+            $modalInstance.close();
+            console.log('from modal',$scope);
+            $scope.__proto__.subject = null;
+            $scope.__proto__.$parent.socialnumber = null;
 
         };
     }]);
 
 angular.module('ndrApp')
-  .controller('ReportController', [
-                 '$scope', '$stateParams', '$state', '$modal', '$filter', 'dataService',
-        function ($scope,   $stateParams,   $state,   $modal,  $filter, dataService) {
+    .controller('ReportController', [
+        '$scope', '$stateParams', '$state', '$modal', '$filter', 'dataService',
+        function($scope, $stateParams, $state, $modal, $filter, dataService) {
 
-        //var account = $scope.accountModel;
-		
-		//console.log("from report", account);
-        console.log('ReportController: Init',  $stateParams.patientID);
-		
-        $scope.socialnumber = '',//'19121212-1212'; //för test
-        $scope.subjectID         = $stateParams.patientID;
-        $scope.view              = 0; // 0 = new view, 1 = report, 2 = incidence
-        $scope.contactToUpdate   = {};
-        $scope.subject           = null;
-        $scope.serverSubjectError = null;
-        $scope.method            = 'POST';
-        $scope.optionalQuestions = [];
-		
-        $scope.filterValue = function($event){
-            if(isNaN(String.fromCharCode($event.keyCode)) && $event.keyCode !== 44 && $event.keyCode !== 46){
-                $event.preventDefault();
-            }
-        };
+            //console.log("from report", account);
 
-        if ($scope.subjectID) {
-            dataService.getSubjectById($scope.subjectID).then(function (subject) {
-				if (subject.socialNumber != undefined) {
-					$scope.socialnumber = subject.socialNumber;
-					$scope.getSubject(true);	
-				}
-            });
-        }
-
-        $scope.getSubject = function (newSocialnumber) {
-			$scope.minYear			  = $scope.socialnumber.substring(0,4);
+            $scope.socialnumber = '', //'19121212-1212'; //för test
+            $scope.subjectID = $stateParams.patientID;
+            $scope.view = $stateParams.view ? $stateParams.view : 0; // 0 = no view, 1 = report, 2 = incidence
+            $scope.contactToUpdate = null;
+            $scope.subject = null;
             $scope.serverSubjectError = null;
-            $scope.serverSaveErrors   = [];
-			
-			$scope.view == null;
-			
-            dataService.getSubjectBySocialNumber($scope.socialnumber)
-                .then(function (data) {
-					
-					if (data.status) { //something has gone wrong
-						$scope.subject=null;
-						$scope.serverSubjectError = 'Ingen patient hittades. Kontrollera personnummer.';
-						return;
-					} else {
-						$scope.serverSubjectError = null;
-					}
+            $scope.method = 'POST';
+            //$scope.optionalQuestions = [];
+            $scope.newIndex = 0;
+            $scope.metafields = null;
+            $scope.isLoading = false
+            $scope.isLoaded = false;
 
-                    $scope.subject = data;
-					$scope.view = 0;
-					
-					setTimeout(function(){ window.document.getElementById('bnNewContact').focus(); }, 100);
+            $scope.filterValue = function($event) {
+                if (isNaN(String.fromCharCode($event.keyCode)) && $event.keyCode !== 44 && $event.keyCode !== 46) {
+                    $event.preventDefault();
+                }
+            };
 
-                    //Välj det senast kompletterade/skapade besöket
-                    /*if (!newSocialnumber) {
-                        $scope.contactToUpdate = $scope.getContactFromContactDate($scope.subject.contacts, $scope.contactModel.contactDate);
-                        $scope.setContact($scope.contactToUpdate);
-                    } else {
-						$scope.contactModel = null;
-                    }*/
-					
-                })
-                ['catch'](function(data, status, headers, config) {
-                    $scope.subject = null;
-					$scope.serverSubjectError = 'Ett okänt fel inträffade';
+            $scope.init = function() {
+
+              var unitTypeID = $scope.accountModel.activeAccount.unit.typeID;
+              $scope.metafields = dataService.getFormFields(null,unitTypeID);
+              if (!$scope.metafields) {
+                $scope.loadQuestions(unitTypeID);
+              } else {
+                $scope.contactQuestions = $scope.getContactQuestions($scope.metafields);
+                $scope.optionalQuestions = $scope.getOptionalQuestions($scope.metafields);
+                $scope.incidenceQuestions = $scope.getIncidenceQuestions($scope.metafields);
+              }
+
+              if (!$scope.subjectID) {
+                $scope.setSocialNumberFieldFocus();
+              }
+
+
+            }
+
+            $scope.setSocialNumberFieldFocus = function() {
+              if (!$scope.subjectID) {
+                try {
+                  window.document.getElementById('socialnumber-input').focus();
+                }
+                catch(err) {
+
+                }
+              }
+            }
+
+            $scope.loadQuestions = function(unitTypeID) {
+
+              $scope.isLoading = true;
+    					dataService.getMetaFields($scope.accountModel.activeAccount.accountID,$scope.accountModel.activeAccount.unit.typeID).then(function () {
+                $scope.isLoading = false
+                $scope.init();
+                return;
+    					});
+
+      			};
+
+            $scope.getContactQuestions = function(questions) {
+              return questions.filter(function(q) {
+                return (q.formID == 1  && !q.isOptional)
+              });
+            };
+            $scope.getOptionalQuestions = function(questions) {
+              return questions.filter(function(q) {
+                return (q.isOptional)
+              });
+            };
+            $scope.getIncidenceQuestions = function(questions) {
+              return questions.filter(function(q) {
+                return (q.formID == 2)
+              });
+            };
+
+            if ($scope.subjectID) {
+                dataService.getSubjectById($scope.subjectID).then(function(subject) {
+                    if (subject.socialNumber != undefined) {
+                        $scope.socialnumber = subject.socialNumber;
+                        $scope.getSubject(true);
+                    }
                 });
-        };
-		
-        $scope.setContact = function (contactToUpdate) {
-			$scope.contactToUpdate = contactToUpdate;
-			$scope.view = 1;
-			
-        };
-		$scope.setIncidence = function() {
-			$scope.view = 2;
-		}
-		
-		
-		/*$scope.calculateAge = function(birthDate, contactDate) {
-		  var age;
+            }
 
-		  var timeDiff = contactDate.valueOf() - birthDate.valueOf();
-		  var milliInDay = 24*60*60*1000;
-		  var noOfDays = timeDiff / milliInDay;
-		  var daysInYear = 365.242; //exact days in year
-		  age =  ( noOfDays / daysInYear ) ;
+            $scope.getSubject = function(newSocialnumber) {
+                $scope.minYear = $scope.socialnumber.substring(0, 4);
+                $scope.serverSubjectError = null;
+                $scope.serverSaveErrors = [];
+                $scope.view == null;
 
-		  return age;
-		};*/
-	
-	//Todo, this could use an overview
-	/*$scope.validateContactDateInput = function() {
-		var date = $scope.contactModel.contactDate.$viewValue;
-		var isValid = true;
-		
-		if ($scope.contactModel.contactDate === undefined) {
-			isValid = false;
-		}
-		
-		isValid = $scope.validateDate(date);
-				
-		$scope.contactForm.contactDate.$setValidity('checkInput',isValid);
-		
-		return isValid;
-		
-	};*/
-		/*$scope.validatePreviousContacts = function() {
-			
-			var isValid = true;
-			var dateToCheck = $scope.getStringDate($scope.contactModel.contactDate);
-			var m = $scope.contactModel;
-			
-			$scope.contactModel.contactDate = dateToCheck.replace('.','-');
+                dataService.getSubjectBySocialNumber($scope.socialnumber)
+                    .then(function(data) {
 
-			var compare = function(thisDate, thatDate) {
-			
-				if (thisDate == thatDate) {
-					return false;
-				}
-					return true;
-			};
+                        console.log('data', data);
 
-			if (!($scope.subject.contacts  === undefined)) {
-				for (var i = 0; i < $scope.subject.contacts.length; i++) {
+                        if (data.status) { //something has gone wrong
+                            $scope.subject = null;
+                            $scope.serverSubjectError = 'Ingen patient hittades. Kontrollera personnummer.';
+                            return;
+                        } else {
+                            $scope.serverSubjectError = null;
+                        }
 
-					var c = $scope.subject.contacts[i];
+                        $scope.subject = data;
+                        $scope.view = 0;
 
-					if($scope.contactToUpdate != null) {
-						if (!(c.contactID == m.contactID)) {
-							isValid = compare(c.contactDate.split('T')[0],dateToCheck)
-						}
-						} else {
-							if ($scope.subject.contacts[i].contactDate.split('T')[0] == dateToCheck) {
-							isValid = compare(c.contactDate.split('T')[0],dateToCheck)
-						}
-					}
+                        setTimeout(function() {
+                            window.document.getElementById('bnNewContact').focus();
+                        }, 100);
 
-					if(!isValid)
-						break;
 
-				}
-			}
+                    })['catch'](function(data, status, headers, config) {
+                        $scope.subject = null;
+                        $scope.serverSubjectError = 'Ett okänt fel inträffade';
+                    });
+            };
 
-			$scope.contactForm.contactDate.$setValidity('checkContactDate', isValid);
-			
-			return isValid;
-			
-		};*/
-		/*$scope.getStringDate = function(date) {
+            $scope.setContact = function(contactToUpdate) {
+                $scope.contactToUpdate = contactToUpdate;
+                $scope.newIndex = $scope.newIndex + 1;
+                $scope.view = 1;
+                $scope.scrollToPatient();
+            };
+            $scope.scrollToPatient = function() {
 
-		  if (typeof date === 'string')
-			return date;
-		  if (date === undefined)
-			return;
-		  if (date === null)
-			return;
-		  if (!(date instanceof Date && !isNaN(date.valueOf())))
-			return
+              var $el = $("#subject");
+              $("html, body").animate({
+                scrollTop: $el.offset().top// + ($el.outerHeight() / 2) - ($(window).height() / 2)
+              },500);
 
-		  var yyyy = date.getFullYear().toString();
-		  var mm = (date.getMonth()+1).toString(); // getMonth() is zero-based
-		  var dd  = date.getDate().toString();
+            }
+            $scope.setIncidence = function() {
+                $scope.view = 2;
+            }
+            $scope.init();
 
-		  return (yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]));
-		};*/
 
-    //not needed for now
-     //$scope.$watch('contactModel.contactDate', function(date){
+            /*$scope.calculateAge = function(birthDate, contactDate) {
+              var age;
 
-      //if (date === undefined)
-      //  return;
+              var timeDiff = contactDate.valueOf() - birthDate.valueOf();
+              var milliInDay = 24*60*60*1000;
+              var noOfDays = timeDiff / milliInDay;
+              var daysInYear = 365.242; //exact days in year
+              age =  ( noOfDays / daysInYear ) ;
 
-      //if (date === null)
-      //  return;
+              return age;
+            };*/
 
-      //$scope.contactModel.contactDate = $scope.getStringDate(date);
+            //Todo, this could use an overview
+            /*$scope.validateContactDateInput = function() {
+            	var date = $scope.contactModel.contactDate.$viewValue;
+            	var isValid = true;
 
-      // console.log(date);
-      // $scope.contactModel.contactDate = $scope.getStringDate(date);
-      // console.log(d);
-      // if (typeof d === 'string')
-        // return;
-      // if (d === undefined)
-        // return;
-      // if (d === null)
-        // return;
+            	if ($scope.contactModel.contactDate === undefined) {
+            		isValid = false;
+            	}
 
-      // var yyyy = d.getFullYear().toString();
-      // var mm = (d.getMonth()+1).toString(); // getMonth() is zero-based
-      // var dd  = d.getDate().toString();
+            	isValid = $scope.validateDate(date);
 
-      // $scope.contactModel.contactDate = yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
-     //});
+            	$scope.contactForm.contactDate.$setValidity('checkInput',isValid);
 
-  }])
+            	return isValid;
+
+            };*/
+            /*$scope.validatePreviousContacts = function() {
+
+            	var isValid = true;
+            	var dateToCheck = $scope.getStringDate($scope.contactModel.contactDate);
+            	var m = $scope.contactModel;
+
+            	$scope.contactModel.contactDate = dateToCheck.replace('.','-');
+
+            	var compare = function(thisDate, thatDate) {
+
+            		if (thisDate == thatDate) {
+            			return false;
+            		}
+            			return true;
+            	};
+
+            	if (!($scope.subject.contacts  === undefined)) {
+            		for (var i = 0; i < $scope.subject.contacts.length; i++) {
+
+            			var c = $scope.subject.contacts[i];
+
+            			if($scope.contactToUpdate != null) {
+            				if (!(c.contactID == m.contactID)) {
+            					isValid = compare(c.contactDate.split('T')[0],dateToCheck)
+            				}
+            				} else {
+            					if ($scope.subject.contacts[i].contactDate.split('T')[0] == dateToCheck) {
+            					isValid = compare(c.contactDate.split('T')[0],dateToCheck)
+            				}
+            			}
+
+            			if(!isValid)
+            				break;
+
+            		}
+            	}
+
+            	$scope.contactForm.contactDate.$setValidity('checkContactDate', isValid);
+
+            	return isValid;
+
+            };*/
+            /*$scope.getStringDate = function(date) {
+
+              if (typeof date === 'string')
+            	return date;
+              if (date === undefined)
+            	return;
+              if (date === null)
+            	return;
+              if (!(date instanceof Date && !isNaN(date.valueOf())))
+            	return
+
+              var yyyy = date.getFullYear().toString();
+              var mm = (date.getMonth()+1).toString(); // getMonth() is zero-based
+              var dd  = date.getDate().toString();
+
+              return (yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]));
+            };*/
+
+            //not needed for now
+            //$scope.$watch('contactModel.contactDate', function(date){
+
+            //if (date === undefined)
+            //  return;
+
+            //if (date === null)
+            //  return;
+
+            //$scope.contactModel.contactDate = $scope.getStringDate(date);
+
+            // console.log(date);
+            // $scope.contactModel.contactDate = $scope.getStringDate(date);
+            // console.log(d);
+            // if (typeof d === 'string')
+            // return;
+            // if (d === undefined)
+            // return;
+            // if (d === null)
+            // return;
+
+            // var yyyy = d.getFullYear().toString();
+            // var mm = (d.getMonth()+1).toString(); // getMonth() is zero-based
+            // var dd  = d.getDate().toString();
+
+            // $scope.contactModel.contactDate = yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
+            //});
+
+        }
+    ])
 
 /*angular.module("ndrApp")
   .service('List', ['$http', 'APIconfigService', function($http, APIconfigService) {
@@ -245,32 +321,32 @@ angular.module('ndrApp')
 
 
 angular.module("ndrApp")
-  .directive('decimals', function() {
-  return {
-    require: 'ngModel',
-    link: function(scope, elm, attrs, ngModel) {
+    .directive('decimals', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, elm, attrs, ngModel) {
 
 
-      if (!ngModel)
-        return;
+                if (!ngModel)
+                    return;
 
-      ngModel.$render = function () {
-        elm.val(ngModel.$viewValue);
-        return true;
-      }
+                ngModel.$render = function() {
+                    elm.val(ngModel.$viewValue);
+                    return true;
+                }
 
-      elm.bind('blur', function() {
-        scope.$apply(read);
-        ngModel.$render();
-      });
+                elm.bind('blur', function() {
+                    scope.$apply(read);
+                    ngModel.$render();
+                });
 
-      read();
+                read();
 
-      function read() {
-        if (elm.val()>0)
-          ngModel.$setViewValue(parseFloat(elm.val()).toFixed(attrs.decimals));
-      }
+                function read() {
+                    if (elm.val() > 0)
+                        ngModel.$setViewValue(parseFloat(elm.val()).toFixed(attrs.decimals));
+                }
 
-    }
-  };
-});
+            }
+        };
+    });
