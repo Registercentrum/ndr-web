f12837bdcc3f4804143839376aaad9d7a83b228a
HenrikMilefors <henrik.milefors@registercentrum.se>
2018-12-11 18:15:43 +0100

    Hook to fetch current location for Google Analytics
    Send current pageview to Google Analytics.

--- src/AppController.js
+++ src/AppController.js
@@ -1,5 +1,7 @@
 angular.module('ndrApp')
-  .controller('AppController', ['accountService', function (accountService) {
+  .controller('AppController', ['accountService','$transitions', function (accountService,$transitions) {
       accountService.bootstrap();
-
+      $transitions.onFinish({}, function($transition) {
+        ga('send', 'pageview', $transition.$to().self.url);
+      });
   }]);
